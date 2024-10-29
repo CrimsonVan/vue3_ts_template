77c9073ec7c0f3167d0c40f372024222021057f5
crimsonvan <2329016850@qq.com>
2024-10-29 17:44:58 +0800

    first

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+function test(args: any) {
+  console.log(args)
+}
+test('hello')
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
